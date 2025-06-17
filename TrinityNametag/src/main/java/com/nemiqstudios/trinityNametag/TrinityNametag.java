bc0140a93c55b8b41319566773ee14669887d25b
Gustavo Soares <gustavosoaresbt@hotmail.com>
2025-06-17 11:45:01 -0300

    feat (TrinityNametag): Agora a tag atualiza em tempo real, sem necessidade de relogar

--- TrinityNametag/src/main/java/com/nemiqstudios/trinityNametag/TrinityNametag.java
+++ TrinityNametag/src/main/java/com/nemiqstudios/trinityNametag/TrinityNametag.java
@@ -1,6 +1,10 @@
 package com.nemiqstudios.trinityNametag;
 
 import net.luckperms.api.LuckPerms;
+import net.luckperms.api.event.EventBus;
+import net.luckperms.api.event.user.UserDataRecalculateEvent;
+import net.luckperms.api.event.user.track.UserPromoteEvent;
+import net.luckperms.api.event.user.track.UserTrackEvent;
 import net.luckperms.api.model.group.Group;
 import net.luckperms.api.model.user.User;
 import org.bukkit.Bukkit;
@@ -14,19 +18,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
-public final class TrinityNametag extends JavaPlugin implements Listener {
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
+public final class TrinityNametag extends JavaPlugin implements Listener {
     private LuckPerms luckPerms;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+
         RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
         if (provider != null) {
             luckPerms = provider.getProvider();
         }
 
         Bukkit.getPluginManager().registerEvents(this, this);
+
+        EventBus eventBus = luckPerms.getEventBus();
+        eventBus.subscribe(this, UserDataRecalculateEvent.class, this::onUserDataRecalculate);
 
         Bukkit.getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "[TrinityNametag] Plugin iniciado com sucesso!");
     }
@@ -112,5 +123,26 @@ public final class TrinityNametag extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
 
         setPlayerTag(player);
+    }
+
+    private final Set<UUID> recentlyUpdated = new HashSet<>();
+
+    private void onUserDataRecalculate(UserDataRecalculateEvent event) {
+        UUID uuid = event.getUser().getUniqueId();
+
+        if (recentlyUpdated.contains(uuid)) return;
+        recentlyUpdated.add(uuid);
+
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            Player player = Bukkit.getPlayer(uuid);
+
+            if (player != null && player.isOnline()) {
+                setPlayerTag(player);
+                this.getLogger().info("Tag atualizada após delay para " + player.getName());
+            }
+
+            // Libera para nova atualização
+            recentlyUpdated.remove(uuid);
+        }, 10L); // Espera 10 ticks (~0.5s)
     }
 }
